bb36d3742c52210ccf4ce22646733f51c149ac03
zeeshan <zeeshanfarrukh82@gmail.com>
2015-05-19 18:29:41 +1000

    added the track by expression to display duplicate entries

--- login.php
+++ login.php
@@ -1,19 +1,20 @@
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
 <html>
 <h3>Current user in System</h3>
-<div ng-app="dbusers" ng-controller="sysUsers">
-<ul>
-  <li ng-repeat="n in names">
-    {{n}}
-  </li>
-</ul>
-</div>
-
 <script>
-var app = angular.module('dbusers', []);0
+var app = angular.module('dbusers', []);
 app.controller('sysUsers', function($scope, $http) {
     $http.get("users.php")
     .success(function(res) {$scope.names = res;});
 });
 </script> 
+<div ng-app="dbusers" ng-controller="sysUsers">
+<ul>
+  <li ng-repeat="n in names track by $index">
+    {{n}}
+  </li>
+</ul>
+</div>
+
+
 
